33c04a4dfd1eb5a03db62403e156dc9c626cbcd1
Konstantin Semenov <kvsemenoff@mail.ru>
2017-01-05 15:19:39 +0300

    d

--- index.php
+++ index.php
@@ -5,11 +5,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Главная</title>
 
-	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
 	<link href="//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet">
 	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
 	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
-	<link rel="stylesheet" href="libs/wow/animate.css">
+
 	<link type="text/css" href="css/jquery.jscrollpane.css" rel="stylesheet" media="all" />
 
 	<link rel="shortcut icon" href="favicon.ico" />
@@ -19,23 +18,44 @@
 <body>
 	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
 	<script src="libs/owl.carousel/owl.carousel.js"></script>
-	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
-	<script src="libs/wow/wow.min.js"></script>
+
 	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
-	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
 
-	<script>
-    	new WOW().init();
-    </script>
-
-	<script src="js/responsiveTabs.js"></script>
 	<script src="js/jquery.maskedinput.min.js"></script>
 	<script src="js/common.js"></script>
 
-	<?php require_once('includes/header.php'); ?>
-	
-	<?php require_once('includes/footer.php'); ?>
+	<section class="header">
+		<div class="container">
+			<div class="header-top">
+				<div class="logo-box">
+					<a href="/"><img src="img/logo.png" alt=""></a>
+				</div>
+				<div class="menu-box">
+					<ul class="main-menu">
+						<li><a href="#">О проекте</a></li>
+						<li><a href="#">тренер</a></li>
+						<li><a href="#">Учебные модули</a></li>
+						<li><a href="#">Записаться на курс</a></li>
+						<li><a href="#">Отзывы</a></li>
+						<li><a href="#">Контакты</a></li>
+						<li><a href="#">Киберблог</a></li>
+					</ul>
+				</div>
+				<div class="clear"></div>
+			</div>
+			<div class="header-middle">
+				<!-- Блок соцсетей -->
+				<div class="soc-block">
+					<span class="soc-block__item soc-block__item_font soc-block__item_position"><i class="fa fa-facebook" aria-hidden="true"></i></span>
+					<span class="soc-block__item soc-block__item_font soc-block__item_position"><i class="fa fa-linkedin" aria-hidden="true"></i></span>	
+					<span class="soc-block__item soc-block__item_font soc-block__item_position"><i class="fa fa-twitter" aria-hidden="true"></i></span>	
+					<span class="soc-block__item soc-block__item_font soc-block__item_position"><i class="fa fa-vk" aria-hidden="true"></i></span>	
+				</div>
 
+			</div>
+		</div>
+	</section>
+                              
 	
 </body>
 </html>
