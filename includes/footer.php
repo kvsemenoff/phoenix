0d929b649e3dee966565e85ad9306b4e67066080
Barasbi <betokov93@gmail.com>
2017-01-05 22:59:38 +0300

    footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,30 @@
 <section class="db-footer">
 	<div class="db-footer-container">
 		<div class="db-h">
-		<h2>КОНТАКТЫ</h2>
+			<h2>КОНТАКТЫ</h2>
 		</div>
-	</div>
-</section>
+		<div class="db-text-content">
+			<div class="db-text-one">
+				Пр.Победителей 12 а
+			</div>
+			<div class="db-text-two">
+				тел:+375 29 756 45 56
+			</div>
+		</div>
+		<div class="clearfix"></div>
+		<div class="db-seti-content">
+			<div class="db-seti-one">
+				<a href="#"><img src="img/db-facebook.png" alt="image"></a>
+			</div>
+			<div class="db-seti-two">
+				<a href="#"><img src="img/db-in.png" alt="image"></a>
+			</div>
+			<div class="db-seti-three">
+				<a href="#"><img src="img/db-vk.png" alt="image"></a>
+			</div>
+			<div class="db-seti-four">
+				<a href="#"><img src="img/db-twitter.png" alt="image"></a>
+				</div>
+			</div>
+		</div>
+	</section>
